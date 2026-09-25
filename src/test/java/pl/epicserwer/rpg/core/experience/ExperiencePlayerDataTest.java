b7db087ad8186635c085bf433e13125708e54043
ddykacz <dawiddykaczd@wp.pl>
2026-09-25 10:29:19 +0200

    fix tests

--- src/test/java/pl/epicserwer/rpg/core/experience/ExperiencePlayerDataTest.java
+++ src/test/java/pl/epicserwer/rpg/core/experience/ExperiencePlayerDataTest.java
@@ -2,91 +2,88 @@ package pl.epicserwer.rpg.core.experience;
 
 import org.commons.experience.Experience;
 import org.commons.experience.Level;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.epicserwer.rpg.core.experience.required.RequiredExperienceService;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ExperiencePlayerDataTest {
 
+    @Mock
     private RequiredExperienceService experienceService;
-
-    @BeforeEach
-    void setUp() {
-        experienceService = new RequiredExperienceService.Builder()
-                .add(1, 100L)
-                .add(2, 300L)
-                .add(3, 600L)
-                .build();
-    }
 
     @Test
     void shouldCreatePlayerDataCorrectly() {
-        // given
         Experience initialExp = new Experience(0L);
         Level initialLevel = new Level(1);
 
-        // when
         ExperiencePlayerData data = new ExperiencePlayerData(initialExp, initialLevel);
 
-        // then
         assertEquals(initialExp, data.getPlayerExperience());
         assertEquals(initialLevel, data.getPlayerLevel());
     }
 
     @Test
     void shouldAddExperienceWithoutLevelUp() {
-        // given
         ExperiencePlayerData data = new ExperiencePlayerData(new Experience(0L), new Level(1));
         Experience addedExp = new Experience(50L);
 
-        // when
+        when(experienceService.calculateRequiredExperience(new Level(1)))
+                .thenReturn(new Experience(100L));
+
         ExperiencePlayerData updatedData = data.addExperience(addedExp, experienceService);
 
-        // then
         assertEquals(new Experience(50L), updatedData.getPlayerExperience());
-        assertEquals(new Level(1), updatedData.getPlayerLevel(), "Poziom nie powinien wzrosnąć (50 < 100)");
-        assertNotSame(data, updatedData, "Metoda powinna zwrócić nowy obiekt (niemutowalność)");
+        assertEquals(new Level(1), updatedData.getPlayerLevel(), "Level should not increase (50 < 100)");
+        assertNotSame(data, updatedData, "Method should return a new instance (immutability)");
     }
 
     @Test
     void shouldAddExperienceAndLevelUpOnce() {
-        // given
         ExperiencePlayerData data = new ExperiencePlayerData(new Experience(80L), new Level(1));
-        Experience addedExp = new Experience(40L); // 80 + 40 = 120
+        Experience addedExp = new Experience(40L);
 
-        // when
+        when(experienceService.calculateRequiredExperience(new Level(1)))
+                .thenReturn(new Experience(100L));
+        when(experienceService.calculateRequiredExperience(new Level(2)))
+                .thenReturn(new Experience(300L));
+
         ExperiencePlayerData updatedData = data.addExperience(addedExp, experienceService);
 
-        // then
         assertEquals(new Experience(120L), updatedData.getPlayerExperience());
         assertEquals(new Level(2), updatedData.getPlayerLevel(),
-                "Poziom powinien wzrosnąć do 2, ponieważ całkowite exp (120) >= 100");
+                "Level should increase to 2 because total exp (120) is >= 100");
     }
 
     @Test
     void shouldAddExperienceAndLevelUpMultipleTimes() {
-        // given
         ExperiencePlayerData data = new ExperiencePlayerData(new Experience(0L), new Level(1));
         Experience addedExp = new Experience(450L);
 
-        // when
+        when(experienceService.calculateRequiredExperience(new Level(1)))
+                .thenReturn(new Experience(100L));
+        when(experienceService.calculateRequiredExperience(new Level(2)))
+                .thenReturn(new Experience(300L));
+        when(experienceService.calculateRequiredExperience(new Level(3)))
+                .thenReturn(new Experience(600L));
+
         ExperiencePlayerData updatedData = data.addExperience(addedExp, experienceService);
 
-        // then
         assertEquals(new Experience(450L), updatedData.getPlayerExperience());
         assertEquals(new Level(3), updatedData.getPlayerLevel(),
-                "Poziom powinien przeskoczyć z 1 na 3 (450 przeskakuje wymóg 100 oraz 300, ale nie dobija do 600)");
+                "Level should jump from 1 to 3 (450 bypasses 100 and 300, but is less than 600)");
     }
 
     @Test
     void shouldBeEqualIfPropertiesAreTheSame() {
-        // given
         ExperiencePlayerData data1 = new ExperiencePlayerData(new Experience(150L), new Level(2));
         ExperiencePlayerData data2 = new ExperiencePlayerData(new Experience(150L), new Level(2));
 
-        // then
         assertEquals(data1, data2);
         assertEquals(data1.hashCode(), data2.hashCode());
     }
